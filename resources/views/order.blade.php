5147db257680a898416ea47102566b60508f37c4
jasonchn18 <jasonchn18@gmail.com>
2022-03-07 16:53:03 +0800

    Add Tailwind CSS, route for Order page, and empty Order page

---
+++ resources/views/order.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    My Orders
+  </h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route for Order view
+Route::view('order', 'order');
