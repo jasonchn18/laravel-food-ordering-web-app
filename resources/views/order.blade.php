9f75db518e365f4a83344fbb8896c36b177ca911
jasonchn18 <jasonchn18@gmail.com>
2022-03-14 23:06:39 +0800

    Remove dark mode from Order page

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -14,7 +14,7 @@
 
     {{-- Orders --}}
     <div class="p-5">
-        <div class="flex flex-row p-5 leading-normal border shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <div class="flex flex-row p-5 leading-normal border shadow-md hover:bg-gray-100">
             <div class="w-1/5">
                 <p> Order: <span class="font-semibold"> &lt;order_id&gt; </span> </p>
                 <p> Status: <span class="font-semibold"> Unpaid </span> </p>
@@ -25,11 +25,11 @@
             </div>
             <div class="w-4/5">
                 <div class="flex flex-col justify-between">
-                <div class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <div class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100">
                     <img class="flex object-cover max-w-sm w-1/6 h-96 rounded-t-lg md:h-28 md:rounded-none md:rounded-l-lg" src="https://dam.kraftheinzcompany.com/adaptivemedia/rendition/195370-3000x2000.jpg?id=093000b4880e99e6cd87fa511235a789145c5a0a&ht=2000&wd=3000&version=1&clid=pim" alt="">
                     <div class="flex flex-col justify-between p-4 leading-normal w-4/6">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> Cheeseburger </h5>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> Quantity: 1 </p>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"> Cheeseburger </h5>
+                        <p class="mb-3 font-normal text-gray-700"> Quantity: 1 </p>
                     </div>
                     <div class="flex justify-center leading-normal w-1/6">
                         <button class="openRemoveModal bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-1 px-3 border-2 border-red-500 hover:border-transparent rounded">
@@ -38,11 +38,11 @@
                     </div>
                 </div>
                 <div class="p-1"></div>
-                <div class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <div class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100">
                     <img class="flex object-cover max-w-sm w-1/6 h-96 rounded-t-lg md:h-28 md:rounded-none md:rounded-l-lg" src="https://cdn.vox-cdn.com/thumbor/dTVDqXGOSiMQwEIozcIj_ByaUlw=/0x0:1000x750/1400x788/filters:focal(0x0:1000x750):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/45140100/6792268281_d0822743b8_b.0.0.jpg" alt="">
                     <div class="flex flex-col justify-between p-4 leading-normal w-4/6">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> Flat White </h5>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> Quantity: 1 </p>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"> Flat White </h5>
+                        <p class="mb-3 font-normal text-gray-700"> Quantity: 1 </p>
                     </div>
                     <div class="flex justify-center leading-normal w-1/6">
                         <button class="openRemoveModal bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-1 px-3 border-2 border-red-500 hover:border-transparent rounded">
@@ -56,7 +56,7 @@
     </div>
 
     <div class="p-5">
-        <div class="flex flex-row p-5 leading-normal border shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <div class="flex flex-row p-5 leading-normal border shadow-md hover:bg-gray-100">
             <div class="w-1/5">
                 <p> Order: <span class="font-semibold"> &lt;order_id&gt; </span> </p>
                 <p> Status: <span class="font-semibold"> Paid </span> </p>
@@ -67,11 +67,11 @@
             </div>
             <div class="w-4/5">
                 <div class="flex flex-col justify-between">
-                <div class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <div class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100">
                     <img class="flex object-cover max-w-sm w-1/6 h-96 rounded-t-lg md:h-28 md:rounded-none md:rounded-l-lg" src="https://dam.kraftheinzcompany.com/adaptivemedia/rendition/195370-3000x2000.jpg?id=093000b4880e99e6cd87fa511235a789145c5a0a&ht=2000&wd=3000&version=1&clid=pim" alt="">
                     <div class="flex flex-col justify-between p-4 leading-normal w-4/6">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> Cheeseburger </h5>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> Quantity: 1 </p>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"> Cheeseburger </h5>
+                        <p class="mb-3 font-normal text-gray-700"> Quantity: 1 </p>
                     </div>
                     <div class="flex justify-center leading-normal w-1/6">
                         <button class="hidden openRemoveModal bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-1 px-3 border-2 border-red-500 hover:border-transparent rounded">
@@ -80,11 +80,11 @@
                     </div>
                 </div>
                 <div class="p-1"></div>
-                <div class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <div class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100">
                     <img class="flex object-cover max-w-sm w-1/6 h-96 rounded-t-lg md:h-28 md:rounded-none md:rounded-l-lg" src="https://cdn.vox-cdn.com/thumbor/dTVDqXGOSiMQwEIozcIj_ByaUlw=/0x0:1000x750/1400x788/filters:focal(0x0:1000x750):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/45140100/6792268281_d0822743b8_b.0.0.jpg" alt="">
                     <div class="flex flex-col justify-between p-4 leading-normal w-4/6">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> Flat White </h5>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> Quantity: 1 </p>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"> Flat White </h5>
+                        <p class="mb-3 font-normal text-gray-700"> Quantity: 1 </p>
                     </div>
                     <div class="flex justify-center leading-normal w-1/6">
                         <button class="hidden openRemoveModal bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-1 px-3 border-2 border-red-500 hover:border-transparent rounded">
@@ -158,10 +158,10 @@
       <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
       <div class="relative px-4 w-full max-w-md h-full md:h-auto">
           <!-- Modal content -->
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div class="relative bg-white rounded-lg shadow">
               <!-- Modal header -->
               <div class="flex justify-end p-2">
-                  <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
+                  <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-toggle="popup-modal">
                       {{-- <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>   --}}
                   </button>
               </div>
@@ -169,12 +169,12 @@
               <div class="p-6 pt-3 text-center">
                   {{-- <svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg> --}}
                   <div class="flex justify-center pb-5">
-                    <svg role="status" class="mr-2 w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-green-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg role="status" class="mr-2 w-16 h-16 text-gray-200 animate-spin fill-green-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                       <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
                     </svg>
                   </div>
-                  <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Payment is being processed.</h3>
+                  <h3 class="mb-5 text-lg font-normal text-gray-500">Payment is being processed.</h3>
                   {{-- <button data-modal-toggle="popup-modal" type="button" class="closePaymentModal text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                       Yes, I'm sure
                   </button>
@@ -189,10 +189,10 @@
       <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
       <div class="relative px-4 w-full max-w-md h-full md:h-auto">
           <!-- Modal content -->
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div class="relative bg-white rounded-lg shadow">
               <!-- Modal header -->
               <div class="flex justify-end p-2">
-                  <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
+                  <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-toggle="popup-modal">
                       {{-- <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>   --}}
                   </button>
               </div>
@@ -204,7 +204,7 @@
                       <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                   </div>
-                  <h3 class="mb-5 text-2xl font-semibold text-gray-600 dark:text-gray-400">Payment successful!</h3>
+                  <h3 class="mb-5 text-2xl font-semibold text-gray-600">Payment successful!</h3>
                   {{-- <button data-modal-toggle="popup-modal" type="button" class="closePaymentModal text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                       Yes, I'm sure
                   </button>
