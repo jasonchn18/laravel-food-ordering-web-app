643c28e89958dbda88819bf2adeb53e901acfb15
jasonchn18 <jasonchn18@gmail.com>
2022-04-05 19:18:35 +0800

    Show orders in Order page with delete function

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -16,10 +16,11 @@
     </h1>
 
     {{-- Orders --}}
+    @foreach ($orders as $order)
     <div class="px-3 py-2">
         <div class="flex flex-row p-4 leading-normal border shadow-md hover:bg-gray-100">
             <div class="p-2 w-1/5">
-                <p> Order: <span class="font-semibold"> &lt;order_id&gt; </span> </p>
+                <p> Order ID: <span class="font-semibold"> {{$order->id}} </span> </p>
                 <p> Status: <span class="font-semibold"> Unpaid </span> </p>
                 <br>
                 <button class="openPaymentModal bg-green-500 hover:bg-green-400 text-white font-semibold py-2 px-6 border-b-4 border-green-700 hover:border-green-500 rounded">
@@ -28,141 +29,30 @@
             </div>
             <div class="w-4/5">
                 <div class="flex flex-col justify-between">
+                    @foreach ($order->food as $food)
+                    <input type="hidden" id="order_id" value={{$order->id}} />
+                    <input type="hidden" id="food_id" value={{$food->id}} />
                     <div class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100">
                         <div class="flex rounded-lg">
-                            <img class="flex h-28 w-44 object-fill rounded-lg" src="https://dam.kraftheinzcompany.com/adaptivemedia/rendition/195370-3000x2000.jpg?id=093000b4880e99e6cd87fa511235a789145c5a0a&ht=2000&wd=3000&version=1&clid=pim" alt="">
+                            <img class="flex h-28 w-44 object-fill rounded-lg" src="{{$food->picture}}" alt="">
                         </div>
                         <div class="flex flex-col place-content-center px-4 py-3 leading-normal w-4/6">
-                                <h5 class="flex mb-2 text-2xl font-bold tracking-tight text-gray-900"> Cheeseburger </h5>
-                                <p class="flex font-normal text-gray-700">Quantity: 1 </p>
+                                <h5 class="flex mb-2 text-2xl font-bold tracking-tight text-gray-900"> {{$food->name}} </h5>
+                                <p class="flex font-normal text-gray-700"> {{$food->pivot->quantity}} </p>
                         </div>
                         <div class="flex justify-center leading-normal w-1/6">
-                            <button class="openRemoveModal text-red-700 font-semibold bg-inherit border-red-500 rounded hover:text-white hover:bg-red-500 hover:border-transparent py-1 px-3 border-2">
-                            <span> Remove </span>
-                            </button>
+                                <button onclick="remove_form_action({{$order->id}}, {{$food->id}})" type="button" class="openRemoveModal text-red-700 font-semibold bg-inherit border-red-500 rounded hover:text-white hover:bg-red-500 hover:border-transparent py-1 px-3 border-2">
+                                    <span> Remove </span>
+                                </button>
                         </div>
                     </div>
                     <div class="p-1"></div>
-                    <div class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100">
-                        <div class="flex rounded-lg">
-                            <img class="flex h-28 w-44 object-fill rounded-lg" src="https://cdn.vox-cdn.com/thumbor/dTVDqXGOSiMQwEIozcIj_ByaUlw=/0x0:1000x750/1400x788/filters:focal(0x0:1000x750):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/45140100/6792268281_d0822743b8_b.0.0.jpg" alt="">
-                        </div>
-                        <div class="flex flex-col justify-between px-4 py-3 leading-normal w-4/6">
-                            <h5 class="flex mb-2 text-2xl font-bold tracking-tight text-gray-900"> Flat White </h5>
-                            <p class="flex font-normal text-gray-700"> Quantity: 1 </p>
-                        </div>
-                        <div class="flex justify-center leading-normal w-1/6">
-                            <button class="openRemoveModal text-red-700 font-semibold bg-inherit border-red-500 rounded hover:text-white hover:bg-red-500 hover:border-transparent py-1 px-3 border-2">
-                            <span> Remove </span>
-                            </button>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
     </div>
-
-    <div class="px-3 py-2">
-        <div class="flex flex-row p-4 leading-normal border shadow-md hover:bg-gray-100">
-            <div class="p-2 w-1/5">
-                <p> Order: <span class="font-semibold"> &lt;order_id&gt; </span> </p>
-                <p> Status: <span class="font-semibold"> Paid </span> </p>
-                <br>
-                <button class="hidden openPaymentModal bg-green-500 hover:bg-green-400 text-white font-semibold py-2 px-6 border-b-4 border-green-700 hover:border-green-500 rounded">
-                  <span class="drop-shadow"> Make Payment </span>
-                </button>
-            </div>
-            <div class="w-4/5">
-                <div class="flex flex-col justify-between">
-                    <div class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100">
-                        <div class="flex rounded-lg">
-                            <img class="flex h-28 w-44 object-fill rounded-lg" src="https://dam.kraftheinzcompany.com/adaptivemedia/rendition/195370-3000x2000.jpg?id=093000b4880e99e6cd87fa511235a789145c5a0a&ht=2000&wd=3000&version=1&clid=pim" alt="">
-                        </div>
-                        <div class="flex flex-col place-content-center px-4 py-3 leading-normal w-4/6">
-                                <h5 class="flex mb-2 text-2xl font-bold tracking-tight text-gray-900"> Cheeseburger </h5>
-                                <p class="flex font-normal text-gray-700">Quantity: 1 </p>
-                        </div>
-                        <div class="flex justify-center leading-normal w-1/6">
-                            <button class="hidden openRemoveModal text-red-700 font-semibold bg-inherit border-red-500 rounded hover:text-white hover:bg-red-500 hover:border-transparent py-1 px-3 border-2">
-                            <span> Remove </span>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="p-1"></div>
-                    <div class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100">
-                        <div class="flex rounded-lg">
-                            <img class="flex h-28 w-44 object-fill rounded-lg" src="https://cdn.vox-cdn.com/thumbor/dTVDqXGOSiMQwEIozcIj_ByaUlw=/0x0:1000x750/1400x788/filters:focal(0x0:1000x750):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/45140100/6792268281_d0822743b8_b.0.0.jpg" alt="">
-                        </div>
-                        <div class="flex flex-col justify-between px-4 py-3 leading-normal w-4/6">
-                            <h5 class="flex mb-2 text-2xl font-bold tracking-tight text-gray-900"> Flat White </h5>
-                            <p class="flex font-normal text-gray-700"> Quantity: 1 </p>
-                        </div>
-                        <div class="flex justify-center leading-normal w-1/6">
-                            <button class="hidden openRemoveModal text-red-700 font-semibold bg-inherit border-red-500 rounded hover:text-white hover:bg-red-500 hover:border-transparent py-1 px-3 border-2">
-                            <span> Remove </span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    {{-- Remove item modal (old) --}}
-    <div class="invisible fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" id="remove-modal_old">
-        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <!--
-                Background overlay, show/hide based on modal state.
-        
-                Entering: "ease-out duration-300"
-                From: "opacity-0"
-                To: "opacity-100"
-                Leaving: "ease-in duration-200"
-                From: "opacity-100"
-                To: "opacity-0"
-            -->
-            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-        
-            <!-- This element is to trick the browser into centering the modal contents. -->
-            {{-- <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span> --}}
-    
-            <!--
-                Modal panel, show/hide based on modal state.
-        
-                Entering: "ease-out duration-300"
-                From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                To: "opacity-100 translate-y-0 sm:scale-100"
-                Leaving: "ease-in duration-200"
-                From: "opacity-100 translate-y-0 sm:scale-100"
-                To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            -->
-            <div class="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full">
-                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div class="p-1 sm:flex sm:items-start">
-                    <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <!-- Heroicon name: outline/exclamation -->
-                      {{-- <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="red" aria-hidden="true">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-                      </svg> --}}
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="-3.5 -3 31 31" stroke="red" strokeWidth={2}>
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                      </svg>
-                    </div>
-                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                      <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Are you sure you want to remove this item from your order?</h3>
-                      {{-- <div class="mt-2">
-                          <p class="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
-                      </div> --}}
-                    </div>
-                </div>
-                </div>
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button type="button" class="closeRemoveModal w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"> Confirm </button>
-                <button type="button" class="closeRemoveModal mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"> Cancel </button>
-                </div>
-            </div>
-        </div>
-    </div>
+    @endforeach
 
     <!-- Remove item modal -->
     <div class="invisible flex h-screen overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full" id="remove-modal">
@@ -190,7 +80,11 @@
                     </div>
                 </div>
                 <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button type="button" class="closeRemoveModal w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"> Confirm </button>
+                    <form name="remove_form" id="remove_form" method="POST" action="/order/{{$order->id}}/{{$food->id}}">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="closeRemoveModal w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"> Confirm </button>
+                    </form>
                     <button type="button" class="closeRemoveModal w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-inherit text-base font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"> Cancel </button>
                 </div>
             </div>
@@ -281,10 +175,10 @@
                 $('#payment-success-modal').addClass('invisible');
               }, 3000);
             }
-
-            // $('.closePaymentModal').on('click', function(e){
-            //     $('#payment-modal').addClass('invisible');
-            // });
         });
+
+        function remove_form_action(order_id, food_id) {
+            $('#remove_form').attr('action', '/order/' + order_id + '/' + food_id);
+        }
     </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\FoodController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,10 +13,12 @@ Auth::routes();
 
 Route::get('logout', [LoginController::class,'logout']);
 
-Route::view('order', 'order');
-
 // food routes with policy
 Route::get('/food/show', [FoodController::class, 'index']);
 Route::post('/food/create', [FoodController::class, 'create']);
 Route::put('/food/{food}', [FoodController::class, 'update']);
 Route::delete('/food/{food}', [FoodController::class, 'destroy']);
+
+// Order routes
+Route::get('order', [OrderController::class, 'show']);
+Route::delete('/order/{order_id}/{food_id}', [OrderController::class, 'destroy']);
