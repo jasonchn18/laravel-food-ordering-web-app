8b773562d60c1ca102e3ed81f8ad3b6eb1e39bc7
jasonchn18 <jasonchn18@gmail.com>
2022-04-06 17:13:50 +0800

    Fix href routs in header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
                     </ul> -->
                     <ul class="navbar-nav ms-auto">
                     <li class="nav-item active">
-                    <a class="nav-link" href="home">Home<span class="sr-only"></a>
+                    <a class="nav-link" href="{{ url('home') }}">Home<span class="sr-only"></a>
                     </li>
                     <!-- disabled link for spacing -->
                     <li class="nav-item disabled">
@@ -50,7 +50,7 @@
                     </li>
 
                     <li class="nav-item active">
-                    <a class="nav-link" href="order">Order <span class="sr-only"></a>
+                    <a class="nav-link" href="{{ url('order') }}">Order <span class="sr-only"></a>
                     </li>
                     <!-- disabled link for spacing -->
                     <li class="nav-item disabled">
@@ -58,7 +58,7 @@
                     </li>
 
                     <li class="nav-item active">
-                    <a class="nav-link" href="cart">
+                    <a class="nav-link" href="{{ url('cart') }}">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                         </svg> 
