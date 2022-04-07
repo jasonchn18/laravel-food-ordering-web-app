2bc457a188bcdb4c220601b44e785c2aa2dea786
jasonchn18 <jasonchn18@gmail.com>
2022-04-08 02:16:53 +0800

    Change routes for home page

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,9 @@ use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-Route::view('/', 'welcome');
-Route::view('/home', 'home');
+// Route::view('/', 'welcome');
+Route::get('/', [FoodController::class, 'index']);
+// Route::view('/home', 'home');
 Route::view('/addfood', 'food.addfood');
 Auth::routes();
 
@@ -16,17 +17,18 @@ Route::get('logout', [LoginController::class,'logout']);
 
 // food routes with policy
 Route::get('/updatefood/{food}', [FoodController::class, 'getForUpdate']);
-Route::get('/food/show', [FoodController::class, 'index']);
+Route::get('/home', [FoodController::class, 'index']);
 Route::get('/food/viewfood', [FoodController::class, 'adminIndex']);
 Route::post('/food/create', [FoodController::class, 'create']);
 Route::get('/food/{food}', [FoodController::class, 'show']);
 Route::post('/food/{food}', [FoodController::class, 'update']);
 Route::delete('/food/{food}', [FoodController::class, 'destroy']);
 
-// Cart routes
-Route::view('cart', 'cart');
-
 // Order routes
 Route::get('order', [OrderController::class, 'show']);
 Route::delete('/order/{order_id}/{food_id}', [OrderController::class, 'destroy']);
+
+// Cart routes
+Route::view('cart', 'cart');
 Route::post('/addToCart', [OrderController::class, 'updateCart']);
+Route::delete('/cart/remove/{food_id}', [OrderController::class, 'removeFromCart']);
