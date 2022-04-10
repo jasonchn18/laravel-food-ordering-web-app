5e95d18533d06256c49c10905b3d3330b592e2f3
hjlew <hueyjinglew4@gmail.com>
2022-04-10 14:59:36 +0800

    Password validation added

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -106,7 +106,7 @@ return [
     'prohibited_if' => 'The :attribute field is prohibited when :other is :value.',
     'prohibited_unless' => 'The :attribute field is prohibited unless :other is in :values.',
     'prohibits' => 'The :attribute field prohibits :other from being present.',
-    'regex' => 'The :attribute format is invalid.',
+    'regex' => 'The :attribute format is invalid. Password require at least 1 uppercase character(A-Z), 1 lower case character(a-z), 1 digit(0-9), 1 non-alphanumeric value (!, $, #, or %)',
     'required' => 'The :attribute field is required.',
     'required_array_keys' => 'The :attribute field must contain entries for: :values.',
     'required_if' => 'The :attribute field is required when :other is :value.',
