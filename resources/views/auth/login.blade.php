e1ff5b4b08217ee829502dc08475b0d83fd5fd52
hjlew <hueyjinglew4@gmail.com>
2022-04-12 16:23:01 +0800

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,10 +34,6 @@
                         </div>
                         <div class="form-group row mb-6">
                             <div class="col-md-6 offset-md-4">
-                                <!-- <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' :'' }}>
-                                    <label class="form-check-label" for="remember">{{ __('Remember Me') }}</label>
-                                </div> -->
                             </div>
                         </div>
                         <div class="form-group row mb-0">
