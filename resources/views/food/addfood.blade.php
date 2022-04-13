619fdf0d690be4cb4c7087132482799cc3b2b977
DrakonDarquesse <mingliangzheng17@gmail.com>
2022-04-14 03:53:37 +0800

    change addfood description placeholder

--- resources/views/food/addfood.blade.php
+++ resources/views/food/addfood.blade.php
@@ -23,7 +23,7 @@
         <div>
           <label for="food description" class="block text-lg font-medium text-gray-700"> Food Description </label>
           <div class="mt-1">
-            <textarea required id="description" name="description" rows="3" class="@error('description') is-invalid @enderror resize-none p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-lg border border-gray-300 rounded-md" placeholder="Cheeseburger not tasty"></textarea>
+            <textarea required id="description" name="description" rows="3" class="@error('description') is-invalid @enderror resize-none p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-lg border border-gray-300 rounded-md" placeholder="Tasty cheeseburger with pickles"></textarea>
             @error('description')
               <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
