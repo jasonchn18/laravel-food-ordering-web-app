df0eef6bde846fd1a1277264b2456c8f83d5256d
DrakonDarquesse <mingliangzheng17@gmail.com>
2022-04-14 03:22:39 +0800

    remove forgot password link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,19 +32,11 @@
                                 @enderror
                             </div>
                         </div>
-                        <div class="form-group row mb-6">
-                            <div class="col-md-6 offset-md-4">
-                            </div>
-                        </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary active">{{ __('Login') }}</button>
-                                @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
-                                @endif
                             </div>
                         </div>
-
                     </form>
                 </div>
             </div>
