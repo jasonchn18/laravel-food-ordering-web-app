00ba55fa5d0f815278e2effee84d787eb5ff5fbb
jasonchn18 <jasonchn18@gmail.com>
2022-04-16 22:30:38 +0800

    Edit delete order modal content

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -58,7 +58,7 @@
             <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
             <div class="relative px-4 w-full max-w-md h-full md:h-auto">
                 <!-- Modal content -->
-                <div class="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full">
+                <div class="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
                     <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                         <div class="p-1 sm:flex sm:items-start">
                             <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -71,7 +71,7 @@
                                 </svg>
                             </div>
                             <div class="pl-1 sm:mt-0 sm:ml-4 sm:text-left">
-                                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Are you sure you want to remove this item from your order?</h3>
+                                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Are you sure you want to delete this order from your order history?</h3>
                                 {{-- <div class="mt-2">
                                                 <p class="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
                                             </div> --}}
