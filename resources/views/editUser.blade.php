b8b0c17bc40ed8dc43befd5eefbc141f42dfdf1a
DrakonDarquesse <mingliangzheng17@gmail.com>
2022-04-16 22:14:18 +0800

    change btn text

--- resources/views/editUser.blade.php
+++ resources/views/editUser.blade.php
@@ -45,7 +45,7 @@
         </div> 
       </div>
       <div class="px-4 py-3 text-right sm:px-6">
-        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-lg font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Add</button>
+        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-lg font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
       </div>
     </div>
   </form>
