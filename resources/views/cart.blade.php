076ebf94e4ca3520c43144460ae0c8766984de44
jasonchn18 <jasonchn18@gmail.com>
2023-01-07 02:47:09 +0800

    Add authorization checking in cart.blade and add alphabetical sorting when sorting by asc or desc food price

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -129,7 +129,9 @@ if (!empty(session('cart'))) {
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="address">
                             Address:
                         </label>
+                        @if(Auth::user())
                         <textarea rows="3" class="resize-none shadow-sm appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline" id="address" name="address">{{Auth::user() -> address}}</textarea>
+                        @endif
                         <span class="">
                             <p id="addresserr" class="text-red-600"></p>
                         </span>
